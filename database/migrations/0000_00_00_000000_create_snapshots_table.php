6701ea1d0b47f5b9485aef64a4e34b879e94b9e7
Mark Kimsal <metrofindings@gmail.com>
2021-12-18 12:29:52 -0500

    fix: default created at value

--- database/migrations/0000_00_00_000000_create_snapshots_table.php
+++ database/migrations/0000_00_00_000000_create_snapshots_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateSnapshotsTable extends Migration
 {
@@ -13,7 +14,7 @@ class CreateSnapshotsTable extends Migration
             $table->uuid('aggregate_uuid');
             $table->unsignedInteger('aggregate_version');
             $table->json('state');
-            $table->timestamp('created_at');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));;
             $table->index('aggregate_uuid');
         });
     }
--- database/migrations/0000_00_00_000000_create_stored_events_table.php
+++ database/migrations/0000_00_00_000000_create_stored_events_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateStoredEventsTable extends Migration
 {
@@ -15,7 +16,7 @@ class CreateStoredEventsTable extends Migration
             $table->string('event_class');
             $table->json('event_properties');
             $table->json('meta_data');
-            $table->timestamp('created_at');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));;
             $table->index('event_class');
             $table->index('aggregate_uuid');
             $table->unique(['aggregate_uuid', 'aggregate_version']);
